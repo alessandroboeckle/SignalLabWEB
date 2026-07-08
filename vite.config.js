ad4154598553d2e3991f90053fa3563abbbdc459
alessandroboeckle <81869814+alessandroboeckle@users.noreply.github.com>
2026-07-08 08:37:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/SignalLabWEB/",
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
+  server: { port: 3000 },
+  build: { outDir: "dist" },
 });
