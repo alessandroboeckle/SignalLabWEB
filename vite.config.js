28731593aee6d67ac339fdb3c7c2193c20d4ce4d
alessandroboeckle <81869814+alessandroboeckle@users.noreply.github.com>
2026-07-08 07:40:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/signal-lab/' : '/',
+  base: "/SignalLabWEB/",
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
-})
+    outDir: "dist",
+    sourcemap: false,
+  },
+});
